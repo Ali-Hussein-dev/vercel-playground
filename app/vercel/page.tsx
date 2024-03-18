56a3abfb1bcb2e1baf1f924002b1cd56eb6a1d3b
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2024-03-18 20:25:44 +0100

    enhance readability

--- app/vercel/page.tsx
+++ app/vercel/page.tsx
@@ -2,9 +2,7 @@
 const Vercel = () => {
   return (
     <div className="bg-slate-800 min-h-screen text-slate-200 px-3">
-      <h1 className="mb-3">Vercel environment variables</h1>
-
-      <div className="space-y-1">
+      <div className="space-y-1 max-w-3xl mx-auto py-4">
       {Object.entries(process.env).map(([k,v])=> (
         <div key={k}>
           <span className="text-slate-400 mr-2">{k}</span> : {v}
