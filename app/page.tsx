65ba715ea2791f67d89629c2d4585644c01198f4
Ali-Hussein-dev <ali.hussein.pre@gmail.com>
2024-03-18 20:17:35 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Next.js Playground</h1>
+      <Link href="/vercel">Vercel Environment Variables</Link>
     </main>
   );
 }
--- app/vercel/page.tsx
+++ app/vercel/page.tsx
@@ -1,10 +1,17 @@
 //======================================
 const Vercel = () => {
-  console.log(process.env);
   return (
-    <div className="">
-      Vercel environment variables
-      <pre>{JSON.stringify(process.env, null, 2)}</pre>
+    <div className="bg-slate-800 min-h-screen text-slate-200 px-3">
+      <h1 className="mb-3">Vercel environment variables</h1>
+
+      <div className="space-y-1">
+      {Object.entries(process.env).map(([k,v])=> (
+        <div key={k}>
+          <span className="text-slate-400 mr-2">{k}</span> : {v}
+          </div>
+      )
+      )}
+      </div>
     </div>
   );
 };
